4d543fd98cb3908729918c374c79537438c94979
Ben Balter <ben.balter@github.com>
2015-12-08 16:57:02 -0500

    initial commit

---
+++ Gemfile
@@ -0,0 +1,6 @@
+source 'https://rubygems.org'
+
+gem 'sinatra'
+gem 'octokit'
+gem 'dotenv'
+gem 'typhoeus'
---
+++ server.rb
@@ -0,0 +1,25 @@
+require 'sinatra'
+require 'octokit'
+require 'dotenv'
+require 'typhoeus'
+require 'json'
+
+Dotenv.load
+
+COLORS = {
+  "good"  => "#FFFFFF",
+  "minor" => "#FFFF00",
+  "major" => "#FF0000"
+}
+
+EVENT_NAME = "github_status_change"
+
+def endpoint
+  "https://maker.ifttt.com/trigger/#{EVENT_NAME}/with/key/#{ENV["IFTTT_MAKER_KEY"]}"
+end
+
+post '/ping' do
+  status = Octokit.github_status.status
+  body = { "value1" => COLORS[status] }
+  Typhoeus.post endpoint, body: body
+end
